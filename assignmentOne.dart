7f3609cd1eaeffb3720a2665a114136041461ed1
Xlroo <victorkenneth.vk@students.uonbi.ac.ke>
2024-04-29 01:07:50 +0300

    First Assignment : Data types

---
+++ assignmentOne.dart
@@ -0,0 +1,10 @@
+void main(){
+  var Patient;
+  String name = "Kinywa";
+  int age = 60;
+  double bSugar = 6.5;
+  num bmi = 24;
+
+  //print patient details 
+  print("$name is a $age year old man ,known diabetic with controlled sugar levels at $bSugar and $bmi BMI");
+}
