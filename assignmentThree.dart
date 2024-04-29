a2c8d99ab1ec2c52971bb4a30b8334dd2afa41e2
Xlroo <victorkenneth.vk@students.uonbi.ac.ke>
2024-04-29 05:49:37 +0300

    Assignment Three : Control Flow Assignment

---
+++ assignmentThree.dart
@@ -0,0 +1,22 @@
+
+import 'dart:io';
+void main(){
+  print("Enter a number:");
+  int? number = int.parse(stdin.readLineSync()!);
+
+  /*If the number is greater than 10, print "Your number is greater than 10"
+If the number is less than 10, print "Your number is less than 10"
+If the number is equal to 10, print "Your number is equal to 10"*/
+
+if (number > 10){
+  print("Your number is greater than 10");
+
+}
+else if (number <10){
+  print("Your number is less than 10");
+}
+else {
+  print("Your number is equal to 10");
+}
+
+}
